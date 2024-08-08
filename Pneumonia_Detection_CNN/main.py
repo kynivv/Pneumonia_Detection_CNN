6e9d2943a796e3f6119bccd136641e1094e8d8bb
devakk <67435419+kynivv@users.noreply.github.com>
2024-08-08 14:06:01 -0400

    Update main.py

--- Pneumonia_Detection_CNN/main.py
+++ Pneumonia_Detection_CNN/main.py
@@ -10,13 +10,14 @@ from keras import layers
 from keras.callbacks import ModelCheckpoint
 
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 
 # Hyperparameters
 IMG_SIZE = 256
 SPLIT = 0.2
-EPOCHS = 25
+EPOCHS = 1
 BATCH_SIZE = 60
 IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
 RANDOM_STATE = 42
@@ -26,8 +27,8 @@ RANDOM_STATE = 42
 X = []
 Y = []
 
-train_dir = 'chest_xray/train'
-test_dir = 'chest_xray/test'
+train_dir = "chest_xray/train"
+test_dir = "chest_xray/test"
 
 classes = os.listdir(train_dir)
 
@@ -37,7 +38,7 @@ resize_list = [train_dir, test_dir]
 
 for p in range(len(resize_list)):
     for i, name in enumerate(classes):
-        images = glob(f'{resize_list[p]}/{name}/*.jpeg')
+        images = glob(f"{resize_list[p]}/{name}/*.jpeg")
 
         for image in images:
             img = cv2.imread(image)
@@ -50,37 +51,55 @@ one_hot_encoded_Y = pd.get_dummies(Y).values
 
 
 # Training Test Split
-X_train, X_test, Y_train, Y_test = train_test_split(X, one_hot_encoded_Y, test_size= SPLIT, random_state= RANDOM_STATE)
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, one_hot_encoded_Y, test_size=SPLIT, random_state=RANDOM_STATE
+)
 
 
 # Creating Model
-base_model = tf.keras.applications.efficientnet.EfficientNetB3(include_top= False, weights= 'imagenet', input_shape= IMG_SHAPE, pooling= 'max')
+base_model = tf.keras.applications.efficientnet.EfficientNetB3(
+    include_top=False, weights="imagenet", input_shape=IMG_SHAPE, pooling="max"
+)
 
-model = keras.Sequential([
-    base_model,
-    layers.BatchNormalization(axis= -1, momentum= 0.99, epsilon= 0.001),
-    layers.Dense(256, activation= 'relu'),
-    layers.Dropout(rate= 0.45, seed= 123),
-    layers.Dense(2, activation= 'softmax')
-])
+model = keras.Sequential(
+    [
+        layers.InputLayer(shape=IMG_SHAPE),
+        base_model,
+        layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001),
+        layers.Dense(256, activation="relu"),
+        layers.Dropout(rate=0.45, seed=123),
+        layers.Dense(2, activation="softmax"),
+    ]
+)
 
-model.compile(optimizer= 'adam', loss= 'categorical_crossentropy', metrics= ['accuracy'])
-print(model.summary())
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+# print(model.summary())
 
 
 # Callbacks
-checkpoint = ModelCheckpoint('Output/model_checkpoint.h5',
-                             save_best_only= True,
-                             verbose= 1,
-                             save_weights_only= True,
-                             monitor= 'val_accuracy')
+checkpoint = ModelCheckpoint(
+    "model_checkpoint.keras",
+    save_best_only=True,
+    verbose=1,
+    save_weights_only=False,
+    monitor="val_accuracy",
+)
 
 
 # Model Training
-history = model.fit(X_train, Y_train,
-                    validation_data= (X_test, Y_test),
-                    batch_size= BATCH_SIZE,
-                    epochs= EPOCHS,
-                    verbose= 1,
-                    callbacks= checkpoint
-                    )
+history = model.fit(
+    X_train,
+    Y_train,
+    validation_data=(X_test, Y_test),
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    verbose=1,
+    callbacks=checkpoint,
+)
+
+model.save("model.keras")
+model.save_weights("model_weights.weights.h5")
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
